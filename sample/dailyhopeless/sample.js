f52a9d0be77291eac883a815b4234375deb74008
nndwn <nandawan.libya@gmail.com>
2023-03-05 06:26:40 +0700

    done section 2

--- sample/dailyhopeless/sample.js
+++ sample/dailyhopeless/sample.js
@@ -1,3 +1,20 @@
-$("#lang").click(function() {
-    $(".lang-menu").toggle()
-})
+// $("#lang").click(function() {
+//     $(".lang-menu").toggle()
+// })
+
+const elementClicked = document.querySelector("#lang");
+const elementYouWantToShow = document.querySelector(".lang-menu");
+
+elementClicked.addEventListener("click", ()=>{
+  elementYouWantToShow.classList.toggle("show");
+});
+
+const resize = document.getElementById('mvArea');
+window.addEventListener('scroll', ()=>{ 
+    if ( window.scrollY > 0) {
+        resize.classList.add("resize");
+    } else {
+        resize.classList.remove("resize");
+    }
+    console.log(scroller);
+});
